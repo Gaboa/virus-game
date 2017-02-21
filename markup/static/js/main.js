3e024e73797f82b374884035ab486a8d35a6fc3c
Dmitry Zhabko <dmitry.zhabko@gmail.com>
2017-02-21 23:27:12 +0200

    Add Init level to main and preload test assets

--- markup/static/js/main.js
+++ markup/static/js/main.js
@@ -1,4 +1,13 @@
 import * as PIXI from 'pixi.js';
+import config from '../../components/config/config';
 import Game from '../../../Core/markup/components/game/game';
+import Init from '../../components/levels/init';
 
 new Game(1024, 768);
+
+game.preload(
+    config.loadArray,
+    'static/img/content/',
+    () => {
+        game.startLevel(Init);
+    });
