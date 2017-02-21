557b99613881fcaef43c3ea29d5d3dacf633d25a
Dmitry Zhabko <dmitry.zhabko@gmail.com>
2017-02-21 23:26:23 +0200

    Add config

---
+++ markup/components/config/config.js
@@ -0,0 +1,32 @@
+export default {
+    loadArray: [
+        {
+            name: 'bg',
+            url: 'bg.png'
+        },
+        {
+            name: 'buttons',
+            url: 'buttons.json'
+        },
+        {
+            name: 'marker',
+            url: 'marker.json'
+        },
+        {
+            name: 'info',
+            url: 'info.json'
+        },
+        {
+            name: 'virus1',
+            url: 'virus1.json'
+        },
+        {
+            name: 'dead',
+            url: 'dead.json'
+        },
+        {
+            name: 'viruses',
+            url: 'viruses.json'
+        }
+    ]
+};
