1810981b43dc729c2777baa1680ca1bc41a232fa
Dmitry Zhabko <dmitry.zhabko@gmail.com>
2017-02-23 19:22:23 +0200

    Add bg and new hitTest logic

--- markup/components/levels/init.js
+++ markup/components/levels/init.js
@@ -1,3 +1,4 @@
+import Sprite from '../../../Core/markup/components/sprite/sprite';
 import Virus from '../virus/virus';
 import Organ from '../organ/organ';
 
@@ -7,6 +8,11 @@ export default class Init {
 
     }
     create() {
+        this.bg = new Sprite({
+            texture: PIXI.utils.TextureCache['bg_1'],
+            anchor: 0
+        });
+
         this.organ = new Organ();
 
         setInterval(() => {
@@ -24,53 +30,26 @@ export default class Init {
     update() {
         this.viruses.forEach((virus) => {
             virus.update();
-            if (this.hitTestRectangle(virus, this.organ.sprite)) {
+
+            if (virus.isCrashed) return;
+            if (virus.isAlive && this.hitTest(virus, this.organ.sprite)) {
+                virus.isCrashed = true;
                 virus.hasToKill = true;
-                console.log('I am killed!');
+                console.log('I must kill', virus);
             }
         });
         this.viruses = this.viruses.filter((virus) => virus.isAlive);
     }
-    hitTestRectangle(r1, r2) {
+    hitTest(sprite1, sprite2) {
+        let hit = false;
+        let deltaX = Math.abs(sprite1.x - sprite2.x);
+        let deltaY = Math.abs(sprite1.y - sprite2.y);
+        let sumOfRadius = sprite1.radius + sprite2.radius;
 
-        let hit, combinedHalfWidths, combinedHalfHeights, vx, vy;
-
-        hit = false;
-
-        r1.centerX = r1.x + r1.width / 2;
-        r1.centerY = r1.y + r1.height / 2;
-        r2.centerX = r2.x + r2.width / 2;
-        r2.centerY = r2.y + r2.height / 2;
-
-        r1.halfWidth = r1.width / 2;
-        r1.halfHeight = r1.height / 2;
-        r2.halfWidth = r2.width / 2;
-        r2.halfHeight = r2.height / 2;
-
-        vx = r1.centerX - r2.centerX;
-        vy = r1.centerY - r2.centerY;
-
-        combinedHalfWidths = r1.halfWidth + r2.halfWidth;
-        combinedHalfHeights = r1.halfHeight + r2.halfHeight;
-
-        if (Math.abs(vx) < combinedHalfWidths) {
-
-            if (Math.abs(vy) < combinedHalfHeights) {
-
-                hit = true;
-
-            } else {
-
-                hit = false;
-
-            }
-        } else {
-
-            hit = false;
-
+        if (deltaX <= sumOfRadius && deltaY <= sumOfRadius) {
+            hit = true;
         }
 
         return hit;
-
     }
 }
