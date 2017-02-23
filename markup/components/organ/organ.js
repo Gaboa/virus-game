63d832ca281b5ffb694c1bf214882ac2f600701d
Dmitry Zhabko <dmitry.zhabko@gmail.com>
2017-02-23 21:00:19 +0200

    Add Organ animations

--- markup/components/organ/organ.js
+++ markup/components/organ/organ.js
@@ -1,25 +1,75 @@
 import Sprite from '../../../Core/markup/components/sprite/sprite';
 
 export default class Organ extends PIXI.Container {
-    constructor() {
+    constructor({
+        name,
+        length
+    }) {
         super();
-        this.radius = 45;
-        this.sprite = new Sprite({
-            texture: PIXI.utils.TextureCache['close.png'],
+        this.name = name;
+        this.length = length;
+        this.health = 6;
+        this.radius = 115;
+        this.isDead = false;
+
+        this.animations = [];
+        this.currentAnimation = 0;
+        this.createOrganAnimations(this.name, this.length, this.health);
+
+        this.buble = new Sprite({
+            texture: PIXI.utils.TextureCache['buble.png'],
+            y: game.view.height / 2,
             x: game.view.width / 2,
-            y: game.view.height / 2
+            container: this
         });
-        this.sprite.radius = this.radius;
+        this.buble.radius = this.radius;
+
         this.circle = new PIXI.Graphics();
         this.circle.beginFill(0x000000, 0.3);
-        this.circle.drawCircle(this.sprite.x, this.sprite.y, this.radius);
+        this.circle.drawCircle(this.buble.x, this.buble.y, this.radius);
         this.circle.endFill();
-        game.stage.addChild(this.circle);
+        // It is debug circle to debug collisions
+        // this.addChild(this.circle);
+
+        game.stage.addChild(this);
     }
     handlers() {
 
     }
     update() {
+        if (this.currentAnimation == 5) {
+            this.isDead = true;
+        }
 
+        if (this.isDead) return;
+
+        if (!this.animations[this.currentAnimation].visible) {
+            this.animations.forEach((animation) => animation.visible = false );
+            this.animations[this.currentAnimation].visible = true;
+            this.animations[this.currentAnimation].play();
+        }
+    }
+    crash() {
+        this.currentAnimation++;
+    }
+    createArrayOfTextures(name, length, prefix = '') {
+        let result = [];
+        for (let i = 1; i <= length; i++) {
+            result.push(PIXI.utils.TextureCache[`${name}_${prefix}${i}.png`]);
+        }
+        return result;
+    }
+    createOrganAnimations(name, length, amount) {
+        for (let i = 1; i <= amount; i++) {
+            let arrayOfTextures = this.createArrayOfTextures(`${name}${i}`, length);
+            let animation = new PIXI.extras.AnimatedSprite(arrayOfTextures);
+            animation.x = game.view.width / 2;
+            animation.y = game.view.height / 2;
+            animation.anchor.set(0.5);
+            animation.visible = false;
+            animation.animationSpeed = 0.15;
+            this.addChild(animation);
+            this.animations.push(animation);
+        }
     }
 }
