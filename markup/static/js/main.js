7466db6590a6fcad64d8bf490703a3f3e6c6b74b
Dmitry Zhabko <dmitry.zhabko@gmail.com>
2017-02-23 19:23:03 +0200

    Change game size

--- markup/static/js/main.js
+++ markup/static/js/main.js
@@ -4,7 +4,7 @@ import config from '../../components/config/config';
 import Game from '../../../Core/markup/components/game/game';
 import Init from '../../components/levels/init';
 
-new Game(1024, 768);
+new Game(900, 1440);
 
 game.preload(
     config.loadArray,
